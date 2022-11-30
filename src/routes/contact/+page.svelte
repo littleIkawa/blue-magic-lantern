d6498d597d71238346876662dc9ee8e2ff096a52
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-30 17:58:59 +0900

    remove mail link

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,20 +4,15 @@
 
 <section class="contact-section section-item">
   <h2 id="contact">Contact</h2>
-  <span>お問い合わせはメールか, TwitterのDMにてお願いいたします。</span>
+  <span>
+    お問い合わせはメールか,
+    TwitterのDMにてお願いいたします。（メールは現在リンクを貼っていませんのでお手数ですが手動でご連絡ください。将来的にフォームを用意する予定です。）
+  </span>
   <table class="contact-table">
     <tbody>
       <tr>
         <th>Mail</th>
-        <td>
-          <a
-            href="mailto:Kw.I.KU.2525@gmail.com?subject=%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B（From Blug-Magic-Lantern）"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Kw.I.KU.2525@gmail.com
-          </a>
-        </td>
+        <td> Kw.I.KU.2525@gmail.com </td>
       </tr>
       <tr>
         <th>Twitter</th>
@@ -30,14 +25,11 @@
     </tbody>
   </table>
   <div class="contact-container">
-    <a
-      href="mailto:Kw.I.KU.2525@gmail.com?subject=%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B（From Blue-Magic-Lantern）"
-    >
-      <div class="contact-item mail">
-        <ServiceIcon name="gmail" size="large" />
-        <span>Mail</span>
-      </div>
-    </a>
+    <div class="contact-item mail">
+      <ServiceIcon name="gmail" size="large" />
+      <span>Mail</span>
+    </div>
+
     <a href="https://twitter.com/wakka141" target="_blank" rel="noopener noreferrer">
       <div class="contact-item twitter">
         <ServiceIcon name="twitter" size="large" />
