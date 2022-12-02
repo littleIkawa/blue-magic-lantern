dde5132a38f7e6b41874561069634312c2ec698a
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-12-02 17:08:26 +0900

    add nested layout and a component

---
+++ src/components/IndexBar.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import PageLink from "./PageLink.svelte";
+
+  interface IndexData {
+    name: string;
+    href: string;
+  }
+  export let indexList: IndexData[];
+</script>
+
+<div class="contents-link-wrapper">
+  {#each indexList as idx}
+    {@const href = idx.href}
+    <PageLink {href} height={"3rem"}>{idx.name}</PageLink>
+  {/each}
+</div>
+
+<style>
+  .contents-link-wrapper {
+    display: flex;
+    justify-content: space-evenly;
+    margin: 4rem 1.4rem;
+    background-image: -webkit-linear-gradient(
+      left,
+      transparent,
+      var(--main-font-color) 8%,
+      var(--main-font-color) 92%,
+      transparent
+    );
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  @media (max-width: 1024px) {
+    .contents-link-wrapper {
+      margin: 2rem 0;
+      position: relative;
+      bottom: 0;
+      height: 3rem;
+    }
+  }
+</style>
---
+++ src/routes/works/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import IndexBar from "@/components/IndexBar.svelte";
+
+  const indexList = [
+    { name: "test1", href: "/works/ekimemo" },
+    { name: "test2", href: "/works/test2" },
+  ];
+</script>
+
+<h2>Works</h2>
+
+作品などを掲載します。
+<IndexBar {indexList} />
+
+<slot />
